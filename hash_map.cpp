6d570a76544263863a15c961580f7a790cd0ffce
Xander Matthew Van Den Nieuwenhuizen <xvandenn@eceprog3.ecn.purdue.edu>
2024-09-19 20:59:46 -0400

    getsize and getcapacity made

--- hash_map.cpp
+++ hash_map.cpp
@@ -7,9 +7,9 @@ hash_map::hash_map(size_t capacity)
 
 hash_map::hash_map(const hash_map &other)
 {
-		int size = other._size;
-		hash_list* arr = new hash_list[size];
-		for(int i = 0; i < size; i++){
+		int cap = other._capacity;
+		hash_list* arr = new hash_list[cap]; //Revert to size if dont work
+		for(int i = 0; i < cap; i++){
 			arr[i] = hash_list(other._head[i]);
 		}
 		
@@ -31,6 +31,7 @@ void hash_map::insert(int key, float value)
 std::optional<float> hash_map::get_value(int key) const
 {
 
+
 }
 
 bool hash_map::remove(int key)
@@ -40,12 +41,12 @@ bool hash_map::remove(int key)
 
 size_t hash_map::get_size() const
 {
-
+	return _size;
 }
 
 size_t hash_map::get_capacity() const
 {
-
+	return _capacity;	
 }
 
 void hash_map::get_all_keys(int *keys)
