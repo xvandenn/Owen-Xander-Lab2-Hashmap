d4a39d951ec04485383cc786edaaeec8b702fde7
owen <owensemeter@gmail.com>
2024-09-19 20:20:42 -0400

    coppied functions over to hash_map.cpp

--- hash_map.cpp
+++ hash_map.cpp
@@ -1,1 +1,56 @@
 #include "hash_map.h"
+
+hash_map(size_t capacity)
+{
+
+}
+
+hash_map(const hash_map &other)
+{
+
+}
+
+hash_map &operator=(const hash_map &other)
+{
+
+}
+
+void insert(int key, float value)
+{
+
+}
+
+std::optional<float> get_value(int key) const
+{
+
+}
+
+bool remove(int key)
+{
+
+}
+
+size_t get_size() const
+{
+
+}
+
+size_t get_capacity() const
+{
+
+}
+
+void get_all_keys(int *keys)
+{
+
+}
+
+void get_bucket_sizes(size_t *buckets)
+{
+
+}
+
+~hash_map()
+{
+
+}
