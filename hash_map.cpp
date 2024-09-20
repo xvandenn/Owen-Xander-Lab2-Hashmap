fe32e9e625877ac77f51dbd8da19bd1bc2f676de
Xander Matthew Van Den Nieuwenhuizen <xvandenn@eceprog3.ecn.purdue.edu>
2024-09-19 20:48:32 -0400

    second const

--- hash_map.cpp
+++ hash_map.cpp
@@ -7,7 +7,15 @@ hash_map::hash_map(size_t capacity)
 
 hash_map::hash_map(const hash_map &other)
 {
-
+		int size = other._size;
+		hash_list* arr = new hash_list[size];
+		for(int i = 0; i < size; i++){
+			arr[i] = hash_list(other._head[i]);
+		}
+		
+		_head = arr;
+		_size = size;
+		_capacity = other._capacity;
 }
 
 hash_map &hash_map::operator=(const hash_map &other)
